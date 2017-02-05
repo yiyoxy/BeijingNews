53a2f65332e392fee9f78bbe3b4b3486f3eb0f0d
tron9527 <278042465@qq.com>
2017-02-05 13:43:31 +0800

    欢迎界面: 三个动画的集合

---
+++ app/src/main/java/cn/tron/beijingnews/WelcomeActivity.java
@@ -0,0 +1,79 @@
+package cn.tron.beijingnews;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import static cn.tron.beijingnews.R.layout.activity_welcome;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private RelativeLayout rl_welcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_welcome);
+
+        initView();
+    }
+
+    // 初始化View
+    private void initView(){
+        rl_welcome = (RelativeLayout) findViewById(R.id.rl_welcome);
+
+        // 三个动画:旋转动画，渐变动画，缩放动画
+        RotateAnimation ra = new RotateAnimation(0, 360,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(2000);
+        ra.setFillAfter(true); //设置停留在旋转后的状态
+
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
+        aa.setDuration(2000);
+        aa.setFillAfter(true);
+
+        ScaleAnimation sa = new ScaleAnimation(0, 1, 0, 1,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        sa.setDuration(2000);
+        sa.setFillAfter(true);
+
+        // 创建动画集合，参数表示他的子动画是否共用一个插值器Interpolator
+        AnimationSet animationSet = new AnimationSet(false);
+        animationSet.addAnimation(ra);
+        animationSet.addAnimation(aa);
+        animationSet.addAnimation(sa);
+
+        // 开始播放动画
+        rl_welcome.startAnimation(animationSet);
+
+        // 监听动画播放完成
+        animationSet.setAnimationListener(new MyAnimationListener());
+    }
+
+    class MyAnimationListener implements Animation.AnimationListener {
+
+        // 当动画开始播放的时候回调
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        // 当动画播放结束的时候回调
+        @Override
+       public void onAnimationEnd(Animation animation) {
+            Toast.makeText(WelcomeActivity.this, "动画播放完成!", Toast.LENGTH_SHORT).show();
+        }
+
+        // 当动画重复播放的时候回调
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+}
