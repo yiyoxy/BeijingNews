be6ba9536b5fd490629af20592a38c6a8f1444ae
tron9527 <278042465@qq.com>
2017-02-05 19:56:27 +0800

    左滑菜单实现的步骤

--- app/src/main/java/cn/tron/beijingnews/activity/MainActivity.java
+++ app/src/main/java/cn/tron/beijingnews/activity/MainActivity.java
@@ -1,15 +1,36 @@
 package cn.tron.beijingnews.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 import cn.tron.beijingnews.R;
+import cn.tron.beijingnews.utils.DensityUtil;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 1.设置主页面
         setContentView(R.layout.activity_main);
+
+        // 2.设置左侧菜单
+        setBehindContentView(R.layout.leftmenu);
+
+        // 3.设置右侧菜单
+        SlidingMenu slidingMenu = getSlidingMenu();
+        //slidingMenu.setSecondaryMenu(R.layout.leftmenu);
+
+        // 4.设置模式: 左侧 + 主页; 左侧 + 主页 + 右侧; 主页 + 右侧
+        slidingMenu.setMode(SlidingMenu.LEFT); // 左侧 + 主页
+
+        // 5.设置滑动的模式: 全屏, 边缘, 不可以滑动
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        // 6.设置主页面占的宽度dip(dp)
+        slidingMenu.setBehindOffset(DensityUtil.dip2px(this, 200));
     }
 }
