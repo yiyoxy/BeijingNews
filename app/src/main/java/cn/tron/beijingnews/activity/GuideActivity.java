43186128029798ed7722bd1fc8e019e98d7d4caf
tron9527 <278042465@qq.com>
2017-02-05 19:14:46 +0800

    添加SlidingMenu的依赖库

--- app/src/main/java/cn/tron/beijingnews/activity/GuideActivity.java
+++ app/src/main/java/cn/tron/beijingnews/activity/GuideActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -44,8 +43,8 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
         ButterKnife.bind(this);
-        Log.e("TAG", "viewpager==" + viewpager);
 
         initData();
     }
