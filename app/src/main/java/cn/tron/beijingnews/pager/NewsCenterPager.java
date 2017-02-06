bf0604d0913950b5ca4720ca3fa5a41c1d319fc7
tron9527 <278042465@qq.com>
2017-02-06 20:06:19 +0800

    扩展练习: 使用系统的api手动解析json数据

--- app/src/main/java/cn/tron/beijingnews/pager/NewsCenterPager.java
+++ app/src/main/java/cn/tron/beijingnews/pager/NewsCenterPager.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
@@ -73,24 +76,24 @@ public class NewsCenterPager extends BasePager {
         x.http().get(params, new Callback.CommonCallback<String>() {
             @Override
             public void onSuccess(String result) {
-                Log.e("TAG","请求成功=="+result);
+                Log.e("TAG", "请求成功==" + result);
 
                 processData(result);
             }
 
             @Override
             public void onError(Throwable ex, boolean isOnCallback) {
-                Log.e("TAG","请求失败=="+ex.getMessage());
+                Log.e("TAG", "请求失败==" + ex.getMessage());
             }
 
             @Override
             public void onCancelled(CancelledException cex) {
-                Log.e("TAG","onCancelled=="+cex.getMessage());
+                Log.e("TAG", "onCancelled==" + cex.getMessage());
             }
 
             @Override
             public void onFinished() {
-                Log.e("TAG","onFinished==");
+                Log.e("TAG", "onFinished==");
             }
         });
     }
@@ -99,7 +102,13 @@ public class NewsCenterPager extends BasePager {
     private void processData(String json) {
 
         // 1.解析数据:手动解析（用系统的Api解析）和第三方解析json的框架（Gson,fastjson）
+
+        // 手动解析
+        // NewsCenterBean centerBean = paraseJson(json);
+
+        // Gson解析
         NewsCenterBean centerBean = new Gson().fromJson(json, NewsCenterBean.class);
+
         dataBeanList = centerBean.getData();
 
         // 2.把新闻中心的数据传递给左侧菜单
@@ -123,6 +132,7 @@ public class NewsCenterPager extends BasePager {
 
     // 根据位置切换到不同的详情页面
     public void switchPager(int preposition) {
+
         // 设置标题
         tv_title.setText(dataBeanList.get(preposition).getTitle());
 
@@ -136,5 +146,85 @@ public class NewsCenterPager extends BasePager {
         fl_main.removeAllViews();
 
         fl_main.addView(rootView);
+    }
+
+    /**
+     * 使用系统的api手动解析json数据
+     * @param json
+     * @return
+     */
+    private NewsCenterBean paraseJson(String json) {
+
+        NewsCenterBean centerBean = new NewsCenterBean();
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            int retcode = jsonObject.optInt("retcode");
+            centerBean.setRetcode(retcode);
+            JSONArray data = jsonObject.optJSONArray("data");
+
+            //数据集合
+            List<NewsCenterBean.DataBean> dataBeans = new ArrayList<>();
+            centerBean.setData(dataBeans);
+
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject itemObject = (JSONObject) data.get(i);
+                if (itemObject != null) {
+
+
+                    //集合装数据
+                    NewsCenterBean.DataBean itemBean = new NewsCenterBean.DataBean();
+                    dataBeans.add(itemBean);
+
+                    int id = itemObject.optInt("id");
+                    itemBean.setId(id);
+                    String title = itemObject.optString("title");
+                    itemBean.setTitle(title);
+                    int type = itemObject.optInt("type");
+                    itemBean.setType(type);
+                    String url = itemObject.optString("url");
+                    itemBean.setUrl(url);
+                    String url1 = itemObject.optString("url1");
+                    itemBean.setUrl1(url1);
+                    String excurl = itemObject.optString("excurl");
+                    itemBean.setExcurl(excurl);
+                    String dayurl = itemObject.optString("dayurl");
+                    itemBean.setDayurl(dayurl);
+                    String weekurl = itemObject.optString("weekurl");
+                    itemBean.setDayurl(weekurl);
+
+                    JSONArray children = itemObject.optJSONArray("children");
+
+                    if (children != null && children.length() > 0) {
+
+
+                        //设置children的数据
+                        List<NewsCenterBean.DataBean.ChildrenBean> childrenBeans = new ArrayList<>();
+                        itemBean.setChildren(childrenBeans);
+                        for (int j = 0; j < children.length(); j++) {
+
+                            NewsCenterBean.DataBean.ChildrenBean  childrenBean = new NewsCenterBean.DataBean.ChildrenBean();
+                            //添加到集合中
+                            childrenBeans.add(childrenBean);
+                            JSONObject childenObje = (JSONObject) children.get(j);
+                            int idc = childenObje.optInt("id");
+                            childrenBean.setId(idc);
+                            String titlec = childenObje.optString("title");
+                            childrenBean.setTitle(titlec);
+                            int typec = childenObje.optInt("type");
+                            childrenBean.setType(typec);
+                            String urlc = childenObje.optString("url");
+                            childrenBean.setUrl(urlc);
+
+                        }
+                    }
+
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return centerBean;
     }
 }
