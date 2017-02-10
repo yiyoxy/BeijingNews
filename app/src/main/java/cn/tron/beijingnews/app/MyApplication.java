d3705b1d717ea0eb195a31c62b7fdc106ed20c17
tron9527 <278042465@qq.com>
2017-02-10 23:46:08 +0800

    集成极光消息推送,从网页发送消息测试成功!

--- app/src/main/java/cn/tron/beijingnews/app/MyApplication.java
+++ app/src/main/java/cn/tron/beijingnews/app/MyApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import org.xutils.BuildConfig;
 import org.xutils.x;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by ZZB27 on 2017.2.6.0006.
  */
@@ -16,5 +18,8 @@ public class MyApplication extends Application {
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
     }
 }
